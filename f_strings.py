ccbcb44a8f50f34970934d67997c88c727004a80
Abuzar-commits <shkhabuzar720@gmail.com>
2026-02-09 17:48:55 +0530

    Add files via upload

---
+++ f_strings.py
@@ -0,0 +1,11 @@
+letter="My name is {0} and I am from {1}"
+name="Abuzar"
+country="India"
+print(letter.format(name,country))
+
+
+#F-strings
+print(f"My name is {name} and I am from {country}")
+
+
+ 

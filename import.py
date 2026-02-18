bf76fb413419f8c67ce115cf86d6f02d89784ee6
Abuzar-commits <shkhabuzar720@gmail.com>
2026-02-18 19:20:16 +0530

    Add files via upload

---
+++ import.py
@@ -0,0 +1,10 @@
+import math
+result=math.sqrt(9)
+print(result)
+from math import sqrt,pi
+result=sqrt(144)
+print(result)
+print(pi)
+#to get all functions from a package we can use *
+import math
+print(dir(math))
